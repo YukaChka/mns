f412adce7b71c86faa8ce2e1ecfa3ccfdda434f4
OneDeadL1ne <mikemp201@gmail.com>
2023-08-08 20:21:29 +0300

    delete web app

--- src/app/api/user/route.ts
+++
@@ -1,8 +0,0 @@
-
-import { NextResponse } from "next/server";
-import {GetUsers} from "./user"
-
-export async function GET(req: Request){
-    const res = await GetUsers();
-    return NextResponse.json(res);
-}
