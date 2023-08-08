7ab81ba32a19770b1ad0d0b4eb579407785d5666
OneDeadL1ne <mikemp201@gmail.com>
2023-08-08 21:04:18 +0300

    update header

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,10 +1,18 @@
 //"use client";
 import style from "./Header.module.css";
-
+import Image from "next/image";
 export default async function Header() {
   return (
     <header>
-      <p>Header</p>
+      <div>
+        <Image
+          src="/next.svg"
+          width={100}
+          height={100}
+          alt="Picture of the author"
+        />
+        <p>Header</p>
+      </div>
     </header>
   );
 }
