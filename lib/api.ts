a19928753d537f9e424c802e57ddbbc80a1b2720
OneDeadL1ne <mikemp201@gmail.com>
2023-08-08 20:23:01 +0300

    create web app

---
+++ lib/api.ts
@@ -0,0 +1,100 @@
+const API_URL = process.env.WORDPRESS_API_URL as string
+import preview from "@/app/api/preview"
+async function fetchAPI(query = '', { variables }: Record<string, any> = {}) {
+  const headers = { 'Content-Type': 'application/json' }
+
+  
+
+  // WPGraphQL Plugin must be enabled
+  const res = await fetch(API_URL, {
+    headers,
+    method: 'POST',
+    body: JSON.stringify({
+      query,
+      variables,
+    }),
+  })
+
+  const json = await res.json()
+  if (json.errors) {
+    console.error(json.errors)
+    throw new Error('Failed to fetch API')
+  }
+  return json.data
+}
+
+export async function getPreviewPost(id:any, idType = 'DATABASE_ID') {
+  const data = await fetchAPI(
+    `
+    query PreviewPost($id: ID!, $idType: PostIdType!) {
+      post(id: $id, idType: $idType) {
+        databaseId
+        slug
+        status
+      }
+    }`,
+    {
+      variables: { id, idType },
+    }
+  )
+  return data.post
+}
+
+export async function getAllPostsWithSlug() {
+  const data = await fetchAPI(`
+    {
+      posts(first: 10000) {
+        edges {
+          node {
+            slug
+          }
+        }
+      }
+    }
+  `)
+  return data?.posts
+}
+
+export async function getAllPostsForHome(preview:any) {
+  const data = await fetchAPI(
+    `
+    query AllPosts {
+      posts(first: 20, where: { orderby: { field: DATE, order: DESC } }) {
+        edges {
+          node {
+            title
+            excerpt
+            slug
+            date
+            featuredImage {
+              node {
+                sourceUrl
+              }
+            }
+            author {
+              node {
+                name
+                firstName
+                lastName
+                avatar {
+                  url
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `,
+    {
+      variables: {
+        onlyEnabled: !preview,
+        preview,
+      },
+    }
+  )
+
+  return data?.posts
+}
+
+
