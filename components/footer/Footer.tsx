880295068b33926bb5c355c23ea10920635eafa1
Yuka <maxzerovol@gmail.com>
2023-08-09 17:45:34 +0300

    Миша покажи попу

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -4,7 +4,26 @@ import style from "./Footer.module.css";
 export default async function Footer() {
   return (
     <footer>
-      <p>Footer</p>
+      <div className={style.bg}>
+        <div className={style.container}>
+          <div className={style.text}>+7 (495) 223-89-86 <br />
+            phonex@megatel.ru
+            <br />
+            <br />
+            1992 - 2023 © Megatel
+          </div>
+          <div className={style.text}>
+            Продукты 
+            <br />
+            Решения
+            <br />
+            О нас
+          </div>
+          <div className={style.text}>
+            @Telegram
+          </div>
+        </div>
+      </div>
     </footer>
   );
 }
