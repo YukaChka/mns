1aeae60c5ce6333b4588dff7a00cc76686fe9ec1
OneDeadL1ne <mikemp201@gmail.com>
2023-08-10 15:43:53 +0300

    add next js fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
-import "./globals.css";
 import type { Metadata } from "next";
 import Header from "@/components/header/Header";
 import Footer from "@/components/footer/Footer";
-
+import { Montserrat } from "next/font/google";
 export const metadata: Metadata = {
   title: "Megatel",
 };
+
+const montserrat = Montserrat({
+  subsets: ["latin", "cyrillic"],
+  weight: ["400", "500", "600", "700", "800"],
+});
 
 export default function RootLayout({
   children,
@@ -14,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body className={montserrat.className}>
         <Header />
         <main>{children}</main>
 
