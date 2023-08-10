6a72703515cd774739c2f9d708690b683a8eb2c7
OneDeadL1ne <mikemp201@gmail.com>
2023-08-10 14:45:20 +0300

    updade footer

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -6,22 +6,20 @@ export default async function Footer() {
     <footer>
       <div className={style.bg}>
         <div className={style.container}>
-          <div className={style.text}>+7 (495) 223-89-86 <br />
+          <div className={style.text}>
+            +7 (495) 223-89-86 <br />
             phonex@megatel.ru
             <br />
             <br />
             1992 - 2023 © Megatel
           </div>
           <div className={style.text}>
-            Продукты 
+            Продукты
             <br />
             Решения
-            <br />
-            О нас
+            <br />О нас
           </div>
-          <div className={style.text}>
-            @Telegram
-          </div>
+          <div className={style.text}>@Telegram</div>
         </div>
       </div>
     </footer>
