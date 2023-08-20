d8faddf7a04c936c74a59fc4df08bc945bf08aca
Yuka <maxzerovol@gmail.com>
2023-08-21 01:38:49 +0300

    up

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -3,45 +3,46 @@ import style from "./Footer.module.css";
 
 export default async function Footer() {
   return (
-    <footer>
-  <div className="flex justify-center">
-        <div className=" grid grid-cols-3 gap-4 content-start max-w-7xl h-10">
-          <div >
+    <footer className="bg-[#009cf3] ">
+      <div className="h-8"></div>
+  <div className="flex container justify-center">
+        <div className=" grid grid-cols-3 gap-24 content-start h-10">
+          <div className="text-white text-[21px]" >
           Продукты
           </div>
 
-          <div >
+          <div className="text-white text-[21px]">
           Решения
           </div>
-          <div>
+          <div className="text-white text-[21px]">
           О нас
           </div>
 
 
         </div>
       </div>
-      <div className="flex justify-center">
-        <div className="gap-4 content-start max-w-7xl">
+      <div className="flex container justify-center">
+        <div className="flex-auto container justify-center">
           
         <div className={style.line}></div>
       </div>
       </div>
-      <div className="flex justify-center">
-        <div className=" grid grid-cols-3 gap-4 content-start max-w-7xl h-10">
 
-            <div className="">
+        <div className=" grid-cols-3 container flex  justify-around ">
+
+            <div className="text-[21px] text-white">
             © 1992 - 2023 Megatel
             </div>
-            <div className="mx-36">
+            <div className="text-white text-[16px]">
             +7 (495) 223-89-86
 phonex@megatel.ru
             </div>
-            <div className="">
+            <div className="text-white text-[21px]">
             @НАШ ЛИНК
             </div>
 
 
-      </div>
+
       </div>
     </footer>
   );
