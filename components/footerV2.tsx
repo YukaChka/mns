edd88455bbfaf2f801354feefe9d96e9ffa18977
OneDeadL1ne <mikemp201@gmail.com>
2023-08-23 13:16:17 +0300

    fix footerV2

--- components/footerV2.tsx
+++ components/footerV2.tsx
@@ -1,8 +1,12 @@
 "use client";
 
+import { useWindowSize } from "@/hooks/useWindowSize";
+
 export const FooterV2 = () => {
+  const { width, height } = useWindowSize();
   return (
     <footer>
+      <div>{width}</div>
       <div></div>
     </footer>
   );
