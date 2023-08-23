3e4315cbb7209abffcd229f6b141b431cc7e0295
OneDeadL1ne <mikemp201@gmail.com>
2023-08-23 20:05:52 +0300

    up footer

--- components/footerV2.tsx
+++ components/footerV2.tsx
@@ -6,12 +6,17 @@ export const FooterV2 = () => {
   const { width, height } = useWindowSize();
   return (
     <footer className="bg-[#009cf3] w-full ">
-
       <li className="flex container justify-center">
         <div className="grid grid-cols-1 md:grid-cols-10  place-items-center m-5 ">
-        <ul className="text-white text-[15px] md:text-[21px] col-start-1 md:col-start-2 col-end-6 ">Продукты</ul>
-        <ul className="text-white text-[15px] md:text-[21px]  col-start-1 md:col-start-6 col-end-7 ">Решения</ul>
-        <ul className="text-white text-[15px] md:text-[21px]  col-start-1 md:col-start-7 col-end-10">О&nbsp;нас</ul>
+          <ul className="text-white text-[15px] md:text-[21px] col-start-1 md:col-start-2 col-end-6 ">
+            Продукты
+          </ul>
+          <ul className="text-white text-[15px] md:text-[21px]  col-start-1 md:col-start-6 col-end-7 ">
+            Решения
+          </ul>
+          <ul className="text-white text-[15px] md:text-[21px]  col-start-1 md:col-start-7 col-end-10">
+            О&nbsp;нас
+          </ul>
         </div>
       </li>
 
@@ -21,12 +26,12 @@ export const FooterV2 = () => {
         </div>
       </div>
 
-      <li className=" grid-cols-3 container flex items-center  justify-around ">
+      <li className=" grid-cols-3 container flex items-center mb-2  justify-around ">
         {width > 970 ? (
           <ul className="text-[21px] text-white">© 1992 - 2023 Megatel</ul>
         ) : null}
 
-        <ul className="text-white text-[10px] sm:text-[15px]">
+        <ul className="text-white text-[18px] sm:text-[21px]">
           +7 (495) 223-89-86 phonex@megatel.ru
         </ul>
         {width > 970 ? (
