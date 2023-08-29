f10f4f26f6fea57694dac1469f66aa2a407a6f29
Yuka <maxzerovol@gmail.com>
2023-08-29 18:40:39 +0300

    up

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -35,7 +35,7 @@ export default function AdminPage() {
               </div>
 
               <div className="mb-3 mt-10">
-                <div className="relative mb-4 flex w-full flex-wrap items-stretch border border-solid">
+                <div className=" mb-4 flex w-full flex-wrap ">
                   <span
                     className="input-group-text flex items-center whitespace-nowrap rounded px-3 py-1.5 text-center text-base font-normal text-neutral-700 dark:text-neutral-200"
                     id="basic-addon2"
@@ -55,7 +55,7 @@ export default function AdminPage() {
                   </span>
                   <Input
                     type="search"
-                    className="relative m-0 block w-[1px] min-w-0 flex-auto rounded  border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
+                    className="relative m-0 block w-[1px] min-w-0 flex-auto rounded  border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out   "
                     placeholder="Search"
                     aria-label="Search"
                     aria-describedby="button-addon2"
@@ -63,11 +63,8 @@ export default function AdminPage() {
                 </div>
               </div>
             </div>
-            <div className=" mt-10 text-2xl">
-              «Мегатель» — российская компания. Мы являемся специализированным
-              поставщиком АСР «ФонексПро», работ по настройке, гарантийной и
-              постгарантийной поддержке, адаптации, доработке и обучению
-              пользователей — напрямую или при участии интеграторов
+            <div className=" mt-10 outline">
+            апцйа
             </div>
           </div>
         </div>
