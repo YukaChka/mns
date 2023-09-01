74a01fc86385d36622b4f5ca3db8dd0cdf43f115
Yuka <maxzerovol@gmail.com>
2023-09-01 13:17:50 +0300

    up

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,7 +1,25 @@
+
+import Image from "next/image";
 export default async function NewsPage() {
   return (
     <main>
-      <p>NewsPage </p>
+          <div className="flex container justify-center">
+            <div className="max-w-6xl w-">
+              <div className="text-xl font-bold">
+                Назад
+              </div>
+              <div className="grid grid-cols-2 ">
+                <div>
+                  Создать новость
+                </div>
+                <div>
+                  Вернуться на главную
+                </div>
+              </div>
+              
+
+            </div>
+          </div>
     </main>
   );
 }
