56107623a5b851e80693b2bbe2206495d0c049e7
OneDeadL1ne <mikemp201@gmail.com>
2023-10-18 22:01:12 +0300

    up config

--- next.config.js
+++ next.config.js
@@ -8,7 +8,12 @@ const nextConfig = {
     });
     return config;
   },
-
+  env: {
+    NEXT_PUBLIC_API: "http://megatelnextjs.ru/api/",
+    NEXTAUTH_SECRET: "pv6WqkDprZKjNigl4CcjQ5lVJRLboheWnluNKPDGIe8=",
+    NEXTAUTH_URL: "http://localhost:3000",
+    NEXT_PUBLIC: "http://localhost:3000",
+  },
   poweredByHeader: false,
 };
 
