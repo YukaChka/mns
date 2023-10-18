6afb26bcfccda63873187bf85444fa8024e0794e
OneDeadL1ne <mikemp201@gmail.com>
2023-10-18 23:23:52 +0300

    test fethc

--- components/admin/tableNews.tsx
+++ components/admin/tableNews.tsx
@@ -3,14 +3,23 @@ import ItemNew from "./itemNew";
 import { useEffect } from "react";
 import { GetStaticProps, InferGetStaticPropsType } from "next";
 
-async function GetData(url: string) {
+async function GetData(url: any) {
   const res = await fetch(url);
 
   return res.json();
 }
 
+const state = process.env.NODE_ENV;
+function GetUrl() {
+  const state = process.env.NODE_ENV;
+  if (state === "production") return "https://megatelnextjs.ru/api/posts";
+
+  return "http://localhost:3000/api/posts";
+}
+
 export default async function TableNews() {
-  const posts = await GetData("/api/posts");
+  const url = GetUrl();
+  const posts = await GetData(url);
   return (
     <div className="">
       {posts.map((post: ItemNewProps) => (
