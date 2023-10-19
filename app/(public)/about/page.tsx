86d108398dc95b7c1dd4218daae95856694f6069
Yuka <maxzerovol@gmail.com>
2023-10-19 17:35:39 +0300

    about ++

--- app/(public)/about/page.tsx
+++ app/(public)/about/page.tsx
@@ -1,7 +1,33 @@
+import Image from "next/image";
+import useDownloader from "react-use-downloader";
+import { UploadForm } from "@/components/forms/UploadForm";
+import NewPreview from "@/components/new-preview";
+
+import { SupportForm } from "@/components/support";
+import { useEffect, useState } from "react";
+
 export default async function AboutPage() {
   return (
     <main>
-      <p>AboutPage</p>
+      <div>
+        <div className="relative">
+
+        <div className="">
+        <Image
+                  src="/img/photo12.jpg"
+                  alt="qe"
+                  className="pointer-events-none"
+                  height={11440}
+                  width={1741}
+                />
+        </div>
+        <div className="absolute text-5xl text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ">
+        qwefwefwegfefg
+        </div>
+        </div>
+
+
+      </div>
     </main>
   );
 }
