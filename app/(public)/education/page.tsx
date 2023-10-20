d81c88c704c54b725edb8d2f92e1546970dbafb5
Yuka <maxzerovol@gmail.com>
2023-10-20 16:06:12 +0300

    Update Я хочу кушать и спать

--- app/(public)/education/page.tsx
+++ app/(public)/education/page.tsx
@@ -1,7 +1,31 @@
 export default async function AboutPage() {
   return (
     <main>
-      <p>AboutPage</p>
+<div className="">
+          <div className="flex container justify-center">
+            <div className="max-w-6xl">
+            <div className="mt-10 text-4xl font-bold">
+            Обучение
+              </div>
+              <div className="mt-10 text-xl font-bold">
+              Осуществляется обучение администрированию и использованию системы, включая:
+              </div>
+              <div className=" mt-10 text-xl">
+                <p className="pb-5">• Настройка, диагностика и развертывание системы.</p>
+                <p className="pb-5">• Использование модулей системы.</p>
+                <p className="pb-5">• Использование модулей системы.</p>
+                <p className="pb-5">• Время реакции на запрос в течение следующего рабочего дня</p>
+                <p className="pb-5">• Подготовка шаблонов отчетов и драйверов.</p>
+              </div>
+              <div className="mt-10 text-xl font-bold">
+              Базовое дистанционное обучение одного специалиста по связи функциям системы включено в стоимость первоначальной поставки системы или нового модуля.
+              </div>
+              <div className="mt-10 text-xl font-bold">
+              Возможен заказ дополнительного обучения
+              </div>
+            </div>
+          </div>
+        </div>
     </main>
   );
 }
