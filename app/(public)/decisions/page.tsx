dc27655fe80e09fec82d8cdedec23b75d00e6b6f
Yuka <maxzerovol@gmail.com>
2023-10-20 18:27:27 +0300

    Update Sex whis Misha

--- app/(public)/decisions/page.tsx
+++ app/(public)/decisions/page.tsx
@@ -1,7 +1,74 @@
 export default async function DecisionPage() {
   return (
     <main>
-      <p>DecisionPage</p>
+      <div className="grid md:grid-cols-2">
+          <div className="flex container justify-center">
+            <div className="max-w-6xl">
+            <div className="mt-10 text-4xl font-bold">
+            Решения
+              </div>
+              <div className="mt-10 text-3xl font-bold">
+              APM модули
+              </div>
+              <div className=" mt-10 text-xl">
+                <p className="pb-5">• APM директор</p>
+                <p className="pb-5">• APM директор</p>
+                <p className="pb-5">• APM директор</p>
+                <p className="pb-5">• APM директор</p>
+              </div>
+              <div className="mt-10 text-3xl font-bold text-[#009CF3]">
+              Службы
+              </div>
+              <div className=" mt-10 text-xl">
+              В данном разделе представлены программные продукты и решения, предлагаемые нашей компанией.
+              Перед покупкой или эксплуатацией наших программных продуктов Вам необходимо ознакомиться с Лицензионным соглашением, которое является неотъемлемой частью программного продукта.
+              Выберите соответствующий раздел для получения более подробной информации по интересующему Вас продукту.
+              </div>
+              <div className="mt-10 text-3xl font-bold text-[#009CF3]">
+              Сертификаты
+              </div>
+              <div className=" mt-10 text-xl">
+                <p className="pb-5">• Сертификат соответствия на ФонексПро 8.0</p>
+                <p className="pb-5">• Сертификат соответствия на ФонексПро 8.0</p>
+                <p className="pb-5">• Сертификат соответствия на ФонексПро 8.0</p>
+                <p className="pb-5">• Сертификат соответствия на ФонексПро 8.0</p>
+              </div>
+            </div>
+          </div>
+          <div className="flex container justify-center">
+            <div className="max-w-6xl">
+            <div className="mt-10 text-4xl font-bold">
+            Продукты
+              </div>
+              <div className="mt-10 text-3xl font-bold text-[#009CF3]">
+              ФонексПро 
+              </div>
+              <div className=" mt-10 text-xl">
+                <p className="pb-5">• ФонексПро 8.0</p>
+                <p className="pb-5">• ФонексПро 7.0.6</p>
+                <p className="pb-5">• Обновления</p>
+                <p className="pb-5">• Поддержка</p>
+              </div>
+              <div className="mt-10 text-3xl font-bold text-[#009CF3]">
+              ФонексПро 
+              </div>
+              <div className=" mt-10 text-xl">
+              В данном разделе представлены программные продукты и решения, предлагаемые нашей компанией.
+              Перед покупкой или эксплуатацией наших программных продуктов Вам необходимо ознакомиться с Лицензионным соглашением, которое является неотъемлемой частью программного продукта.
+              Выберите соответствующий раздел для получения более подробной информации по интересующему Вас продукту.
+              </div>
+              <div className="mt-10 text-3xl font-bold text-[#009CF3]">
+              Сертификаты
+              </div>
+              <div className=" mt-10 text-xl">
+                <p className="pb-5">• Сертификат соответствия на ФонексПро 8.0</p>
+                <p className="pb-5">• Сертификат соответствия на ФонексПро 8.0</p>
+                <p className="pb-5">• Сертификат соответствия на ФонексПро 8.0</p>
+                <p className="pb-5">• Сертификат соответствия на ФонексПро 8.0</p>
+              </div>
+            </div>
+          </div>
+        </div>
     </main>
   );
 }
--- middleware.ts
+++ middleware.ts
@@ -40,6 +40,6 @@ export default withAuth(
     )
     
 
-export const config = { matcher: ["/posts","/orders","/account"] }
+export const config = { matcher: [] }
 
 
