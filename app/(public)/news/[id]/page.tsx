7796eda0a9ef94880f5e095cfbe6a5f5338cf372
OneDeadL1ne <mikemp201@gmail.com>
2023-10-21 20:19:54 +0300

    up new page

--- app/(public)/news/[id]/page.tsx
+++ app/(public)/news/[id]/page.tsx
@@ -1,13 +1,30 @@
 "use client";
 
+import { ItemNewProps } from "@/app/api/posts/posts";
+
 type NewProps = {
   params: {
     id: string;
   };
 };
 
+async function GetPost(id: string) {
+  let url = process.env.NEXT_PUBLIC_BASE_URL;
+  if (!url) {
+    url = `http://megatelnextjs.ru/api/posts?id=${id}`;
+  } else {
+    url = `${process.env.NEXT_PUBLIC_BASE_URL}/api/posts?id=${id}`;
+  }
 
-export default function New({ params }: NewProps) {
+  const responce = await fetch(url);
+
+  return responce.json() as Promise<ItemNewProps>;
+}
+
+export default async function New({ params }: NewProps) {
+  const post = await GetPost(params.id);
+
+  console.log(post);
   return (
     <>
       <p>{params.id}</p>
