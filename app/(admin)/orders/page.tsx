5fcbcf9c385df30fc479c2d94a78056fa94c3377
OneDeadL1ne <mikemp201@gmail.com>
2023-10-21 14:27:27 +0300

    fix fetch

--- app/(admin)/orders/page.tsx
+++ app/(admin)/orders/page.tsx
@@ -1,9 +1,22 @@
-import { OrderTable } from "@/components/admin/OrderTable";
+import { OrderProps } from "@/app/api/order/order";
+import { OrderTablePro } from "@/components/table";
 
+async function GetOrders() {
+  let url = process.env.NEXT_PUBLIC_BASE_URL;
+  if (!url) {
+    url = "http://megatelnextjs.ru/api/order";
+  } else {
+    url = `${url}/api/order`;
+  }
+  const responce = await fetch(url);
+
+  return responce.json() as Promise<OrderProps[]>;
+}
 export default async function AdminOrdersPage() {
+  const data = await GetOrders();
   return (
     <div>
-      <OrderTable />
+      <OrderTablePro data={data} />
     </div>
   );
 }
