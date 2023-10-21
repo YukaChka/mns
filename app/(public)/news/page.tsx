a6a890a6b66cb620d15987e0c8cfa0e8940a5150
Yuka <maxzerovol@gmail.com>
2023-10-21 19:39:30 +0300

    qweeyrtuip;/.ckd stdtrauh arthwEG weg cvb erhueryRET GWGVFSE WAETREGWERASSGRWASJHRATJERw terjtrahuerhtrjreajh

--- app/(public)/news/page.tsx
+++ app/(public)/news/page.tsx
@@ -1,9 +1,41 @@
 "use client";
 import Image from "next/image";
+import useDownloader from "react-use-downloader";
+import { UploadForm } from "@/components/forms/UploadForm";
+import NewPreview from "@/components/new-preview";
+import { ItemNewProps } from "@/app/api/posts/posts";
+import { SupportForm } from "@/components/support";
+import { useEffect, useState } from "react";
+import { isAsync } from "zod";
+async function getpost() {
+  let url = process.env.NEXT_PUBLIC_BASE_URL;
+  if (!url) {
+    url = "http://megatelnextjs.ru/api/posts";
+  } else {
+    url = ${url}/api/posts;
+  }
+  const res = await fetch(url);
+
+  return res.json() as Promise<ItemNewProps[]>;
+}
 export default async function NewsPage() {
+  const posts = await getpost()
   return (
     <main>
-      <p>NewsPage</p>
+      <div className="mt-20">
+        <div className="mb-5 ">
+          <div className="flex container justify-center">
+            <div className="max-w-6xl">
+              <div className=" text-2xl  mt-8">
+                {posts.map}
+               <NewPreview />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+      </div>
+    </div>
     </main>
   );
 }
