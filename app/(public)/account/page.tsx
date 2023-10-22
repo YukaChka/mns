e754c6e5485138a59cc8f1123ee99a2ef4a4d009
Yuka <maxzerovol@gmail.com>
2023-10-22 18:36:51 +0300

    update

--- app/(public)/account/page.tsx
+++ app/(public)/account/page.tsx
@@ -1,6 +1,13 @@
 "use client";
-
+import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import TableNews from "@/components/admin/tableNews";
+import Link from "next/link";
+import { useState } from "react";
+import { CreatePostToast } from "@/components/toasts/CreatePostToast";
+import { ItemNewProps } from "@/app/api/posts/posts";
+import ItemNew from "@/components/admin/itemNew";
 import { signOut, useSession } from "next-auth/react";
 
 export default function AccountPage() {
@@ -10,17 +17,31 @@ export default function AccountPage() {
   return (
     <main>
       <div>
-        {user?.email}
-        {user?.firstname}
-        {user?.lastname}
-        {user?.role}
-      </div>
-      <div>
-        <Button
-          onClick={() => signOut({ callbackUrl: `${window.location.origin}` })}
+      <div className="mt-10">
+          <div className="flex container justify-center">
+            <div className="max-w-6xl min-w-full">
+
+            <div className="mt-10 text-4xl font-bold grid md:grid-cols-2 ">
+            <div>Личный кабинет</div>
+            <div className="flex justify-center md:justify-end">
+              <Button className="bg-[#009CF3]" onClick={() => signOut({ callbackUrl: `${window.location.origin}` })}
         >
           Выйти
-        </Button>
+        </Button></div>
+              </div>
+              <div className="mt-10 text-xl font-bold text-[#009CF3]">
+              Личные данные
+              </div>
+              <div className=" mt-10 text-xl">
+                <p className="pb-5">Фамилия: {user?.lastname}</p> 
+                <p className="pb-5">Имя: {user?.firstname}</p>
+                <p className="pb-5">Почта: {user?.email}</p>
+              </div>
+              </div>
+              </div>
+            </div>
+          </div>
+      <div>
       </div>
     </main>
   );
