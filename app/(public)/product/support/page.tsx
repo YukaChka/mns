87fb5eb9d5c43a87fdc7cf1bc4e6d47432ac8f53
Yuka <maxzerovol@gmail.com>
2023-10-23 22:43:06 +0300

    паь

--- app/(public)/product/support/page.tsx
+++ app/(public)/product/support/page.tsx
@@ -22,27 +22,80 @@ export default async function SupportPage() {
               Стандартная поддержка:
               </div>
               <div className=" mt-10 text-xl">
-                <p className="pb-5">• Обязательное обновление системы до актуальной версии 1 раз в год либо чаще по требованиям регулятора</p>
-                <p className="pb-5">• Предоставление обновления по запросу клиента</p>
-                <p className="pb-5">• Бесплатное устранение недостатков работы заявленного функционала системы</p>
-                <p className="pb-5">• Время реакции на запрос в течение следующего рабочего дня</p>
-                <p className="pb-5">• Дистанционные консультации по телефону или другим каналам связи по правильному использованию программного обеспечения (в рамках соблюдения эксплуатационных требований системы; не включают консультации по эксплуатации оборудования связи, серверного или системного программного обеспечения), включая удаленный терминальный доступ</p>
-                <p className="pb-5">• Перспективные рекомендации по дальнейшей модернизации АСР</p>
-                <p className="pb-5">• Консультации в режиме «горячей линии» до 2 часов в месяц</p>
+              <p className="pb-5 flex justify-start place-items-center">
+                <div className="h-[5px] w-[5px] mr-1 rounded-full bg-[#009CF3] "></div>
+                Обязательное обновление системы до актуальной версии 1 раз в год либо чаще по требованиям регулятора
+              </p>
+              <p className="pb-5 flex justify-start place-items-center">
+                <div className="h-[5px] w-[5px] mr-1 rounded-full bg-[#009CF3] "></div>
+                Предоставление обновления по запросу клиента
+              </p>
+              <p className="pb-5 flex justify-start place-items-center">
+                <div className="h-[5px] w-[5px] mr-1 rounded-full bg-[#009CF3] "></div>
+                Бесплатное устранение недостатков работы заявленного функционала системы
+              </p>
+              <p className="pb-5 flex justify-start place-items-center">
+                <div className="h-[5px] w-[5px] mr-1 rounded-full bg-[#009CF3] "></div>
+                Время реакции на запрос в течение следующего рабочего дня
+              </p>
+              <p className="pb-5 flex justify-start place-items-center">
+                <div className="h-[5px] w-[5px] mr-1 rounded-full bg-[#009CF3] "></div>
+                Дистанционные консультации по телефону или другим каналам связи по правильному использованию программного обеспечения (в рамках соблюдения эксплуатационных требований системы; не включают консультации по эксплуатации оборудования связи, серверного или системного программного обеспечения), включая удаленный терминальный доступ
+              </p>
+              <p className="pb-5 flex justify-start place-items-center">
+                <div className="h-[5px] w-[5px] mr-1 rounded-full bg-[#009CF3] "></div>
+                Перспективные рекомендации по дальнейшей модернизации АСР
+              </p>
+              <p className="pb-5 flex justify-start place-items-center">
+                <div className="h-[5px] w-[5px] mr-1 rounded-full bg-[#009CF3] "></div>
+                Консультации в режиме «горячей линии» до 2 часов в месяц
+              </p>
+
               </div>
               <div className="mt-10 text-3xl text-[#009CF3] font-bold">
               VIP-поддержка
               </div>
               <div className=" mt-10 text-xl">
-                <p className="pb-5">• Неограниченная настройка системы в период настройки или расширения</p>
-                <p className="pb-5">• Расширенное обучение пользователей системы</p>
-                <p className="pb-5">• Регулярный мониторинг и выработка профилактических мер</p>
-                <p className="pb-5">• Срочное восстановление системы после сбоя оборудования и системного ПО, либо при нарушении требований по эксплуатации</p>
-                <p className="pb-5">• Приоритетное обновление</p>
-                <p className="pb-5">• Неограниченные консультации в режиме «горячей линии»</p>
-                <p className="pb-5">• Сокращенное время реакции в течение 4 рабочих часов</p>
-                <p className="pb-5">• Предоплаченный объем дополнительных работ</p>
-                <p className="pb-5">• Расширенные дистанционные консультации</p>
+              <p className="pb-5 flex justify-start place-items-center">
+                <div className="h-[5px] w-[5px] mr-1 rounded-full bg-[#009CF3] "></div>
+                Неограниченная настройка системы в период настройки или расширения
+              </p>
+              <p className="pb-5 flex justify-start place-items-center">
+                <div className="h-[5px] w-[5px] mr-1 rounded-full bg-[#009CF3] "></div>
+                Расширенное обучение пользователей системы
+              </p>
+              <p className="pb-5 flex justify-start place-items-center">
+                <div className="h-[5px] w-[5px] mr-1 rounded-full bg-[#009CF3] "></div>
+                Регулярный мониторинг и выработка профилактических мер
+              </p>
+              <p className="pb-5 flex justify-start place-items-center">
+                <div className="h-[5px] w-[5px] mr-1 rounded-full bg-[#009CF3] "></div>
+                Срочное восстановление системы после сбоя оборудования и системного ПО, либо при нарушении требований по эксплуатации
+              </p>
+              <p className="pb-5 flex justify-start place-items-center">
+                <div className="h-[5px] w-[5px] mr-1 rounded-full bg-[#009CF3] "></div>
+                Приоритетное обновление
+              </p>
+              <p className="pb-5 flex justify-start place-items-center">
+                <div className="h-[5px] w-[5px] mr-1 rounded-full bg-[#009CF3] "></div>
+                Неограниченные консультации в режиме «горячей линии»
+              </p>
+              <p className="pb-5 flex justify-start place-items-center">
+                <div className="h-[5px] w-[5px] mr-1 rounded-full bg-[#009CF3] "></div>
+                Сокращенное время реакции в течение 4 рабочих часов
+              </p>
+              <p className="pb-5 flex justify-start place-items-center">
+                <div className="h-[5px] w-[5px] mr-1 rounded-full bg-[#009CF3] "></div>
+                Сокращенное время реакции в течение 4 рабочих часов
+              </p>
+              <p className="pb-5 flex justify-start place-items-center">
+                <div className="h-[5px] w-[5px] mr-1 rounded-full bg-[#009CF3] "></div>
+                Предоплаченный объем дополнительных работ
+              </p>
+              <p className="pb-5 flex justify-start place-items-center">
+                <div className="h-[5px] w-[5px] mr-1 rounded-full bg-[#009CF3] "></div>
+                Расширенные дистанционные консультации
+              </p>
               </div>
               <div className=" mt-10 text-xl font-bold mb-10">
                 <p>
