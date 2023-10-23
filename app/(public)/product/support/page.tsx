073e914ee0e2bb27b25213b54a82e3c4804079e9
Yuka <maxzerovol@gmail.com>
2023-10-23 23:09:47 +0300

    wfw

--- app/(public)/product/support/page.tsx
+++ app/(public)/product/support/page.tsx
@@ -22,32 +22,32 @@ export default async function SupportPage() {
               Стандартная поддержка:
               </div>
               <div className=" mt-10 text-xl">
-              <p className="pb-5 flex justify-start place-items-center">
-                <div className="h-[5px] w-[5px] mr-1 rounded-full bg-[#009CF3] "></div>
+              <p className="pb-5 flex justify-start ">
+                <div className="h-[5px] w-[5px] mr-1 p-1 mt-[10px] rounded-full bg-[#009CF3] "></div>
                 Обязательное обновление системы до актуальной версии 1 раз в год либо чаще по требованиям регулятора
               </p>
-              <p className="pb-5 flex justify-start place-items-center">
-                <div className="h-[5px] w-[5px] mr-1 rounded-full bg-[#009CF3] "></div>
+              <p className="pb-5 flex justify-start ">
+                <div className="h-[5px] w-[5px] mr-1 p-1 mt-[10px] rounded-full bg-[#009CF3] "></div>
                 Предоставление обновления по запросу клиента
               </p>
-              <p className="pb-5 flex justify-start place-items-center">
-                <div className="h-[5px] w-[5px] mr-1 rounded-full bg-[#009CF3] "></div>
+              <p className="pb-5 flex justify-start ">
+                <div className="h-[5px] w-[5px] mr-1 p-1 mt-[10px] rounded-full bg-[#009CF3] "></div>
                 Бесплатное устранение недостатков работы заявленного функционала системы
               </p>
-              <p className="pb-5 flex justify-start place-items-center">
-                <div className="h-[5px] w-[5px] mr-1 rounded-full bg-[#009CF3] "></div>
+              <p className="pb-5 flex justify-start ">
+                <div className="h-[5px] w-[5px] mr-1 p-1 mt-[10px] rounded-full bg-[#009CF3] "></div>
                 Время реакции на запрос в течение следующего рабочего дня
               </p>
-              <p className="pb-5 flex justify-start place-items-center">
-                <div className="h-[5px] w-[5px] mr-1 rounded-full bg-[#009CF3] "></div>
+              <p className="pb-5 flex justify-start ">
+                <div className="h-[5px] w-[5px] mr-1 p-1 mt-[10px] rounded-full bg-[#009CF3] "></div>
                 Дистанционные консультации по телефону или другим каналам связи по правильному использованию программного обеспечения (в рамках соблюдения эксплуатационных требований системы; не включают консультации по эксплуатации оборудования связи, серверного или системного программного обеспечения), включая удаленный терминальный доступ
               </p>
-              <p className="pb-5 flex justify-start place-items-center">
-                <div className="h-[5px] w-[5px] mr-1 rounded-full bg-[#009CF3] "></div>
+              <p className="pb-5 flex justify-start ">
+                <div className="h-[5px] w-[5px] mr-1 p-1 mt-[10px] rounded-full bg-[#009CF3] "></div>
                 Перспективные рекомендации по дальнейшей модернизации АСР
               </p>
-              <p className="pb-5 flex justify-start place-items-center">
-                <div className="h-[5px] w-[5px] mr-1 rounded-full bg-[#009CF3] "></div>
+              <p className="pb-5 flex justify-start ">
+                <div className="h-[5px] w-[5px] mr-1 p-1 mt-[10px] rounded-full bg-[#009CF3] "></div>
                 Консультации в режиме «горячей линии» до 2 часов в месяц
               </p>
 
@@ -56,44 +56,44 @@ export default async function SupportPage() {
               VIP-поддержка
               </div>
               <div className=" mt-10 text-xl">
-              <p className="pb-5 flex justify-start place-items-center">
-                <div className="h-[5px] w-[5px] mr-1 rounded-full bg-[#009CF3] "></div>
+              <p className="pb-5 flex justify-start ">
+                <div className="h-[5px] w-[5px] mr-1 p-1 mt-[10px] rounded-full bg-[#009CF3] "></div>
                 Неограниченная настройка системы в период настройки или расширения
               </p>
-              <p className="pb-5 flex justify-start place-items-center">
-                <div className="h-[5px] w-[5px] mr-1 rounded-full bg-[#009CF3] "></div>
+              <p className="pb-5 flex justify-start ">
+                <div className="h-[5px] w-[5px] mr-1 p-1 mt-[10px] rounded-full bg-[#009CF3] "></div>
                 Расширенное обучение пользователей системы
               </p>
-              <p className="pb-5 flex justify-start place-items-center">
-                <div className="h-[5px] w-[5px] mr-1 rounded-full bg-[#009CF3] "></div>
+              <p className="pb-5 flex justify-start ">
+                <div className="h-[5px] w-[5px] mr-1 p-1 mt-[10px] rounded-full bg-[#009CF3] "></div>
                 Регулярный мониторинг и выработка профилактических мер
               </p>
-              <p className="pb-5 flex justify-start place-items-center">
-                <div className="h-[5px] w-[5px] mr-1 rounded-full bg-[#009CF3] "></div>
+              <p className="pb-5 flex justify-start ">
+                <div className="h-[5px] w-[5px] mr-1 p-1 mt-[10px] rounded-full bg-[#009CF3] "></div>
                 Срочное восстановление системы после сбоя оборудования и системного ПО, либо при нарушении требований по эксплуатации
               </p>
-              <p className="pb-5 flex justify-start place-items-center">
-                <div className="h-[5px] w-[5px] mr-1 rounded-full bg-[#009CF3] "></div>
+              <p className="pb-5 flex justify-start ">
+                <div className="h-[5px] w-[5px] mr-1 p-1 mt-[10px] rounded-full bg-[#009CF3] "></div>
                 Приоритетное обновление
               </p>
-              <p className="pb-5 flex justify-start place-items-center">
-                <div className="h-[5px] w-[5px] mr-1 rounded-full bg-[#009CF3] "></div>
+              <p className="pb-5 flex justify-start ">
+                <div className="h-[5px] w-[5px] mr-1 p-1 mt-[10px] rounded-full bg-[#009CF3] "></div>
                 Неограниченные консультации в режиме «горячей линии»
               </p>
-              <p className="pb-5 flex justify-start place-items-center">
-                <div className="h-[5px] w-[5px] mr-1 rounded-full bg-[#009CF3] "></div>
+              <p className="pb-5 flex justify-start ">
+                <div className="h-[5px] w-[5px] mr-1 p-1 mt-[10px] rounded-full bg-[#009CF3] "></div>
                 Сокращенное время реакции в течение 4 рабочих часов
               </p>
-              <p className="pb-5 flex justify-start place-items-center">
-                <div className="h-[5px] w-[5px] mr-1 rounded-full bg-[#009CF3] "></div>
+              <p className="pb-5 flex justify-start ">
+                <div className="h-[5px] w-[5px] mr-1 p-1 mt-[10px] rounded-full bg-[#009CF3] "></div>
                 Сокращенное время реакции в течение 4 рабочих часов
               </p>
-              <p className="pb-5 flex justify-start place-items-center">
-                <div className="h-[5px] w-[5px] mr-1 rounded-full bg-[#009CF3] "></div>
+              <p className="pb-5 flex justify-start ">
+                <div className="h-[5px] w-[5px] mr-1 p-1 mt-[10px] rounded-full bg-[#009CF3] "></div>
                 Предоплаченный объем дополнительных работ
               </p>
-              <p className="pb-5 flex justify-start place-items-center">
-                <div className="h-[5px] w-[5px] mr-1 rounded-full bg-[#009CF3] "></div>
+              <p className="pb-5 flex justify-start ">
+                <div className="h-[5px] w-[5px] mr-1 p-1 mt-[10px] rounded-full bg-[#009CF3] "></div>
                 Расширенные дистанционные консультации
               </p>
               </div>
--- app/(public)/services/page.tsx
+++ app/(public)/services/page.tsx
@@ -2,8 +2,8 @@ export default async function ServicesPage() {
   return (
     <main>
       <div className="">
-          <div className="flex container justify-center">
-            <div className="max-w-6xl">
+        <div className="flex container justify-center">
+          <div className="min-h-full max-w-6xl">
             <div className="mt-10 text-4xl font-bold">
             Дополнительные услуги
               </div>
@@ -11,15 +11,42 @@ export default async function ServicesPage() {
               Возможно выполнение различных дополнительных работ, необходимых для организации работы системы «под ключ», включая:
               </div>
               <div className=" mt-10 text-xl">
-                <p className="pb-5">• Полную настройку системы под нужды Заказчика</p>
-                <p className="pb-5">• Адаптацию драйверов оборудования связи</p>
-                <p className="pb-5">• Подготовку шаблонов отчетов и документов</p>
-                <p className="pb-5">• Ввод сведений об оборудовании связи, абонентах, линиях (портах)</p>
-                <p className="pb-5">• Создание тарифных планов</p>
-                <p className="pb-5">• Обеспечение взаимодействия АСР ФонексПро со сторонними системами (активное управление оборудованием связи, гостиничным оборудованием, оборудованием управления доступом, информационно-справочные системы, CRM-системы, системы взаиморасчета с операторами связи, аналитические и SIEM-системы и т.п.)</p>
-                <p className="pb-5">• Миграция системы</p>
-                <p className="pb-5">• Экспертная диагностика и проверка функционирования АСР, сети связи, выявление аномалий в работе сети связи и потребляемых услугах</p>
-                <p className="pb-5">• Разработка дополнительных модулей и интерфейсов для специальных нужд Заказчика</p>
+              <p className="pb-5 flex justify-start ">
+                <div className="h-[5px] w-[5px] mr-1 p-1 mt-[10px] rounded-full bg-[#009CF3] "></div>
+                Полную настройку системы под нужды Заказчика
+              </p>
+              <p className="pb-5 flex justify-start ">
+                <div className="h-[5px] w-[5px] mr-1 p-1 mt-[10px] rounded-full bg-[#009CF3] "></div>
+                Адаптацию драйверов оборудования связи
+              </p>
+              <p className="pb-5 flex justify-start ">
+                <div className="h-[5px] w-[5px] mr-1 p-1 mt-[10px] rounded-full bg-[#009CF3] "></div>
+                Подготовку шаблонов отчетов и документов
+              </p>
+              <p className="pb-5 flex justify-start ">
+                <div className="h-[5px] w-[5px] mr-1 p-1 mt-[10px] rounded-full bg-[#009CF3] "></div>
+                Ввод сведений об оборудовании связи, абонентах, линиях (портах)
+              </p>
+              <p className="pb-5 flex justify-start ">
+                <div className="h-[5px] w-[5px] mr-1 p-1 mt-[10px] rounded-full bg-[#009CF3] "></div>
+                Создание тарифных планов
+              </p>
+              <p className="pb-5 flex justify-start ">
+                <div className="h-[5px] w-[5px] mr-1 p-1 mt-[10px] rounded-full bg-[#009CF3] "></div>
+                Обеспечение взаимодействия АСР ФонексПро со сторонними системами (активное управление оборудованием связи, гостиничным оборудованием, оборудованием управления доступом, информационно-справочные системы, CRM-системы, системы взаиморасчета с операторами связи, аналитические и SIEM-системы и т.п.)
+              </p>
+              <p className="pb-5 flex justify-start ">
+                <div className="h-[5px] w-[5px] mr-1 p-1 mt-[10px] rounded-full bg-[#009CF3] "></div>
+                Миграция системы
+              </p>
+              <p className="pb-5 flex justify-start ">
+                <div className="h-[5px] w-[5px] mr-1 p-1 mt-[10px] rounded-full bg-[#009CF3] "></div>
+                Экспертная диагностика и проверка функционирования АСР, сети связи, выявление аномалий в работе сети связи и потребляемых услугах
+              </p>
+              <p className="pb-5 flex justify-start ">
+                <div className="h-[5px] w-[5px] mr-1 p-1 mt-[10px] rounded-full bg-[#009CF3] "></div>
+                Разработка дополнительных модулей и интерфейсов для специальных нужд Заказчика
+              </p>
               </div>
             </div>
           </div>
