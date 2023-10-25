7eda0ccc1130d0aa4e03fd17fdaaf6a7171a4138
OneDeadL1ne <mikemp201@gmail.com>
2023-10-25 17:25:57 +0300

    fix circle

--- app/(public)/product/[id]/page.tsx
+++ app/(public)/product/[id]/page.tsx
@@ -12,7 +12,6 @@ type ProductProps = {
 export default async function Decision({ params: { id } }: ProductProps) {
   return (
     <main>
-      
       <div className="mt-7">
         <div className="flex container justify-center">
           <div className="max-w-6xl mt-10">
@@ -20,34 +19,41 @@ export default async function Decision({ params: { id } }: ProductProps) {
               {"<"} Продукты
             </Link>
             <div className="mt-5 text-4xl font-bold">Линейка ФонексПро</div>
-            <div className="mt-5 text-3xl font-bold text-[#009CF3]">
+            <div className="mt-7 text-3xl font-bold text-[#009CF3]">
               Все актуальные версии ФонексПро:
             </div>
-            <div className=" mt-10 text-xl">
-              <p className="pb-5">
-                • Опираются на опыт разработчика АСР с 1992 года.
+            <div className=" mt-5 text-xl">
+              <p className="pb-5 flex justify-start ">
+                <div className="h-[5px] w-[5px] mr-1 p-1 mt-[10px] rounded-full bg-[#009CF3] "></div>
+                Опираются на опыт разработчика АСР с 1992 года.
               </p>
-              <p className="pb-5">
-                • Обладают действующим сертификатом Системы сертификации в
-                области связи (№ ОС-3-СТ-0550, ТУ 4251-001-69603108-2016), и
+
+              <p className="pb-5 flex justify-start ">
+                <div className="h-[5px] w-[5px] mr-1 p-1 mt-[10px] rounded-full bg-[#009CF3] "></div>
+                Обладают действующим сертификатом Системы сертификации в области
+                связи (№ ОС-3-СТ-0550, ТУ 4251-001-69603108-2016), и
                 соответствуют установленным требованиям «Правила применения
                 автоматизированных систем расчетов», утвержденным Приказом
                 Мининформсвязи России от 02.07.2007 № 73.
               </p>
-              <p className="pb-5">
-                • Совместимы как с современным, так и с основным унаследованным
+              <p className="pb-5 flex justify-start ">
+                <div className="h-[5px] w-[5px] mr-1 p-1 mt-[10px] rounded-full bg-[#009CF3] "></div>
+                Совместимы как с современным, так и с основным унаследованным
                 оборудованием связи, и адаптируемы к широкому спектру
                 оборудования.
               </p>
-              <p className="pb-5">
-                • Поставляются с одним годом бесплатной стандартной поддержки.
+              <p className="pb-5 flex justify-start ">
+                <div className="h-[5px] w-[5px] mr-1 p-1 mt-[10px] rounded-full bg-[#009CF3] "></div>
+                Поставляются с одним годом бесплатной стандартной поддержки.
               </p>
-              <p className="pb-5">
-                • Совместимы с новыми решениями для специалистов по связи и
+              <p className="pb-5 flex justify-start ">
+                <div className="h-[5px] w-[5px] mr-1 p-1 mt-[10px] rounded-full bg-[#009CF3] "></div>
+                Совместимы с новыми решениями для специалистов по связи и
                 безопасности — модулями «Аналитика» и «АнтиФрод».
               </p>
-              <p className="pb-5">
-                • Разработаны в России без участия иностранного капитала
+              <p className="pb-5 flex justify-start ">
+                <div className="h-[5px] w-[5px] mr-1 p-1 mt-[10px] rounded-full bg-[#009CF3] "></div>
+                Разработаны в России без участия иностранного капитала
                 (соответствует стратегии по реализации импортозамещения).
               </p>
             </div>
@@ -62,24 +68,27 @@ export default async function Decision({ params: { id } }: ProductProps) {
                   Проектная версия 8.0
                 </div>
                 <Button
-                        variant="default"
-                        className="mt-5 middle none center rounded-lg bg-[#D9D9D9] hover:bg-[#cecece] py-3.5 px-7 font-sans text-sm font-bold uppercase text-black shadow-md shadow-[#f5f5f5] transition-all hover:shadow-lg hover:shadow-[#D9D9D9] focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-                        data-ripple-light="true"
-                      >
-                        Документация
-                      </Button>
+                  variant="default"
+                  className="mt-5 middle none center rounded-lg bg-[#D9D9D9] hover:bg-[#cecece] py-3.5 px-7 font-sans text-sm font-bold uppercase text-black shadow-md shadow-[#f5f5f5] transition-all hover:shadow-lg hover:shadow-[#D9D9D9] focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                  data-ripple-light="true"
+                >
+                  Документация
+                </Button>
                 <div className=" mt-10 text-xl">
-                  <p className="pb-5">
-                    <mark>• </mark> Модульная автоматизированная система расчетов для
+                  <p className="pb-5 flex justify-start ">
+                    <div className="h-[5px] w-[5px] mr-1 p-1 mt-[10px] rounded-full bg-[#009CF3] "></div>
+                    Модульная автоматизированная система расчетов для
                     специальных конфигураций, многопользовательской работы или
                     больших сетей связи.
                   </p>
-                  <p className="pb-5">
-                    <mark>• </mark> Является продолжением линеек 5.0, 6.0 и 7.0.
+                  <p className="pb-5 flex justify-start ">
+                    <div className="h-[5px] w-[5px] mr-1 p-1 mt-[10px] rounded-full bg-[#009CF3] "></div>
+                    Является продолжением линеек 5.0, 6.0 и 7.0.
                   </p>
-                  <p className="pb-5">
-                  <mark>• </mark> Представляет собой службу ядро (системную службу Windows),
-                    к которой подключаются специализированные рабочие места
+                  <p className="pb-5 flex justify-start ">
+                    <div className="h-[5px] w-[5px] mr-1 p-1 mt-[10px] rounded-full bg-[#009CF3] "></div>
+                    Представляет собой службу ядро (системную службу Windows), к
+                    которой подключаются специализированные рабочие места
                     («Директор», «Клерк», «Отчеты», «Документы», «Платежи»,
                     «Сверка», «Карты», «Детализация netflow»), модули и
                     дополнительные службы (Гостиница, RADIUS, интеграция с
@@ -87,23 +96,28 @@ export default async function Decision({ params: { id } }: ProductProps) {
                     справочник», «Веб-статистика/Личный кабинет», «Веб-платежи»,
                     новые — «Веб-Аналитика», «АнтиФрод»).
                   </p>
-                  <p className="pb-5">
-                    • Отвечает повышенным требованиям к отказоустойчивости.
+                  <p className="pb-5 flex justify-start ">
+                    <div className="h-[5px] w-[5px] mr-1 p-1 mt-[10px] rounded-full bg-[#009CF3] "></div>
+                    Отвечает повышенным требованиям к отказоустойчивости.
                   </p>
-                  <p className="pb-5">
-                    • Использует внешний SQL-cервер баз данных.
+                  <p className="pb-5 flex justify-start ">
+                    <div className="h-[5px] w-[5px] mr-1 p-1 mt-[10px] rounded-full bg-[#009CF3] "></div>
+                    Использует внешний SQL-cервер баз данных.
                   </p>
-                  <p className="pb-5">
-                    • Позволяет активно управлять оборудованием связи
-                    (авторизация доступа к услугам связи, прекращение соединений
-                    и т.п.)
+                  <p className="pb-5 flex justify-start ">
+                    <div className="h-[5px] w-[5px] mr-1 p-1 mt-[10px] rounded-full bg-[#009CF3] "></div>
+                    Позволяет активно управлять оборудованием связи (авторизация
+                    доступа к услугам связи, прекращение соединений и т.п.)
                   </p>
-                  <p className="pb-5">
-                    • Широкие возможности интеграции с ERP, CRM-системами, СКУД
-                    и т.п.
+                  <p className="pb-5 flex justify-start ">
+                    <div className="h-[5px] w-[5px] mr-1 p-1 mt-[10px] rounded-full bg-[#009CF3] "></div>
+                    Широкие возможности интеграции с ERP, CRM-системами, СКУД и
+                    т.п.
                   </p>
-                  <p className="pb-5">
-                    • Оптимизирована для сетей голосовой связи от 500 до 100 000
+
+                  <p className="pb-5 flex justify-start ">
+                    <div className="h-[5px] w-[5px] mr-1 p-1 mt-[10px] rounded-full bg-[#009CF3] "></div>
+                    Оптимизирована для сетей голосовой связи от 500 до 100 000
                     абонентов и более, либо сетей передачи данных.
                   </p>
                 </div>
